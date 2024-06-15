197e6b8f1f08ff8d702a7dea4f26a377c39854df
Junaid Shaikh <75204207+Junaid2727@users.noreply.github.com>
2024-06-15 21:50:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello from junaid shaikh to all'));
+app.get('/', (req, res) => res.send('Hello from junaid shaikh to all and thank you'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
